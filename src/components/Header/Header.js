d6acb5dd01d219c0a837c52eb7e270138ed0788d
Harun <mharunurrashid777@gmail.com>
2022-08-30 12:59:21 +0600

    first commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,13 @@ import './Header.css';
 const Header = () => {
     return (
         <div className='header'>
-            <img src={logo} alrt=''/>
+            <img src={logo} alt=""/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/review">Order Review</a>
+                <a href="/manage">Manage Inventory</a>
+                
+            </nav>
         </div>
     );
 };
